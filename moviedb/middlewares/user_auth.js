5b3860b737ac63e95ac09dbebb1fd368497fdb2e
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-02-28 20:45:51 +0530

    added debugging

--- moviedb/middlewares/user_auth.js
+++ moviedb/middlewares/user_auth.js
@@ -2,9 +2,10 @@ const isLoggedIn = (req, res, next) => {
 
 
     // req.session.user ? next() : res.redirect('/user/register');
+    console.log("headers",req.headers)
     if(!req.session.user && req.headers && req.headers.Authorization=="Admin")
     {
-        console.log("headers",req.headers)
+        
         req.session.user= {
             _id: "63f855955ff48db5e63fdae1",
              username: 'esakki' }
