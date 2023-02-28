4d1de592be669ad9975eb3e73b137e424d58fd35
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-02-28 18:05:18 +0530

    updated middleware

--- moviedb/middlewares/user_auth.js
+++ moviedb/middlewares/user_auth.js
@@ -7,6 +7,11 @@ const isLoggedIn = (req, res, next) => {
         req.session.user= {
             _id: "63f855955ff48db5e63fdae1",
              username: 'esakki' }
+
+        res.locals.user={
+            _id: "63f855955ff48db5e63fdae1",
+             username: 'esakki' }
+
     }
     next();
 }
