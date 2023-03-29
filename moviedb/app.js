537151dd64cb62eda6b5337f90b4a76c2740dc94
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-03-29 18:56:36 +0530

    updated PORT number

--- moviedb/app.js
+++ moviedb/app.js
@@ -78,7 +78,7 @@ app.use(flash());
 app.use(helmet({ contentSecurityPolicy: false}));
 
 const db=process.env.DATABASE;
-const port=5000
+const port=process.env.PORT
 app.use((req, res, next) => {
 
   console.log("Request for server",req.session);
